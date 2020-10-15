ce2e4964486d074e609c7da7cf78b44665d26931
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 21:42:25 +0800

    [FizzBuzz] [Alfred] Refactor: created gameFizzBuzzWhizz function to avoid redundancy of codes.

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,91 +5,52 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-    @Test
-    void should_return_Fizz_when_count_off_given_order_number_3() {
+    void gameFizzBuzzWhizz(int orderNumber, String expectedValue) {
         //given
-        int orderNumber = 3;
         FizzBuzz fizzBuzz = new FizzBuzz();
         //when
         String given = fizzBuzz.countOff(orderNumber);
         //then
-        assertEquals("Fizz", given);
+        assertEquals(expectedValue, given);
+    }
+
+    @Test
+    void should_return_Fizz_when_count_off_given_order_number_3() {
+        gameFizzBuzzWhizz(3, "Fizz");
     }
 
     @Test
     void should_return_Buzz_when_count_off_given_order_number_5() {
-        //given
-        int orderNumber = 5;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String given = fizzBuzz.countOff(orderNumber);
-        //then
-        assertEquals("Buzz", given);
+        gameFizzBuzzWhizz(5, "Buzz");
     }
 
     @Test
     void should_return_Whizz_when_count_off_given_order_number_7() {
-        //given
-        int orderNumber = 7;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String given = fizzBuzz.countOff(orderNumber);
-        //then
-        assertEquals("Whizz", given);
+        gameFizzBuzzWhizz(7, "Whizz");
     }
 
     @Test
     void should_return_FizzBuzz_when_count_off_given_order_number_multiple_to_3_and_5() {
-        //given
-        int orderNumber = 15;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String given = fizzBuzz.countOff(orderNumber);
-        //then
-        assertEquals("FizzBuzz", given);
+        gameFizzBuzzWhizz(15, "FizzBuzz");
     }
 
     @Test
     void should_return_FizzWhizz_when_count_off_given_order_number_multiple_to_3_and_7() {
-        //given
-        int orderNumber = 21;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String given = fizzBuzz.countOff(orderNumber);
-        //then
-        assertEquals("FizzWhizz", given);
+        gameFizzBuzzWhizz(21, "FizzWhizz");
     }
 
     @Test
     void should_return_BuzzWhizz_when_count_off_given_order_number_multiple_to_5_and_7() {
-        //given
-        int orderNumber = 35;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String given = fizzBuzz.countOff(orderNumber);
-        //then
-        assertEquals("BuzzWhizz", given);
+        gameFizzBuzzWhizz(35, "BuzzWhizz");
     }
 
     @Test
     void should_return_FizzBuzzWhizz_when_count_off_given_order_number_multiple_to_3_and_5_and_7() {
-        //given
-        int orderNumber = 105;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String given = fizzBuzz.countOff(orderNumber);
-        //then
-        assertEquals("FizzBuzzWhizz", given);
+        gameFizzBuzzWhizz(105, "FizzBuzzWhizz");
     }
 
     @Test
     void should_return_actual_number_when_count_off_given_number_is_not_multiple_to_special_number() {
-        //given
-        int orderNumber = 1;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String given = fizzBuzz.countOff(orderNumber);
-        //then
-        assertEquals(String.valueOf(orderNumber), given);
+        gameFizzBuzzWhizz(1, String.valueOf(1));
     }
 }
