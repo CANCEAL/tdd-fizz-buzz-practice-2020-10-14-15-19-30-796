dd8cb9d316ad14058bfb230ba2fbb9b86390bd99
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 14:10:19 +0800

    [FizzBuzz] [Alfred] Refactor: declared MODULO_7 and WHIZZ final variable

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final int MODULO_5 = 5;
     private static final String BUZZ = "Buzz";
+    private static final int MODULO_7 = 7;
+    private static final String WHIZZ = "Whizz";
 
     public String countOff(int orderNumber) {
         if (orderNumber%MODULO_3 == 0) {
@@ -13,8 +15,8 @@ public class FizzBuzz {
         if (orderNumber%MODULO_5 == 0) {
             return BUZZ;
         }
-        if (orderNumber%7 == 0) {
-            return "Whizz";
+        if (orderNumber%MODULO_7 == 0) {
+            return WHIZZ;
         }
         return null;
     }
