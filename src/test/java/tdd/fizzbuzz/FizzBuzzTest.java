ec3ee9ccf5918e285672bb68070c3d39e31e14d5
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 13:56:31 +0800

    [FizzBuzz] [Alfred] Test: case when given is 3 and output is Fizz

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_Fizz_when_count_off_given_order_number_3() {
+        //given
+        int orderNumber = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String given = fizzBuzz.countOff(orderNumber);
+        //then
+        assertEquals("Fizz", given);
+    }
 }
